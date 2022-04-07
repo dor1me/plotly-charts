8db5ec1795851da825ed1b7c64060a163b1b119c
dor1me <99968745+dor1me@users.noreply.github.com>
2022-04-07 15:02:35 +0300

    Add files via upload

--- scatter-4.js
+++ scatter-4.js
@@ -1,4 +1,4 @@
-function scatter_4(){
+function scatter(id){
     var trace1 = {
         y: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
         mode: 'markers',
@@ -6,17 +6,18 @@ function scatter_4(){
           size: 40,
           color: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
         }
-      };
+    };
       
-      var data = [trace1];
-      
-      var layout = {
-        title: 'Scatter Plot with a Color Dimension'
-      };
-      
-      Plotly.newPlot('4', data, layout);
-    }
+    var data = [trace1];
     
-    document.addEventListener('DOMContentLoaded', function(){ 
-        scatter_4();
-    })
+    var layout = {
+        title: 'Scatter Plot with a Color Dimension'
+    };
+    
+    Plotly.newPlot(id, data, layout);
+}
+    
+document.addEventListener('DOMContentLoaded', function(){ 
+    scatter('4');
+    scatter('5');
+})
